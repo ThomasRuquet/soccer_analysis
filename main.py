5d8d76c7d35cb05ea300e6c6ea813b152dd05109
Thomas <thomas.ruquet@outlook.com>
2026-09-15 17:50:57 -0400

    Tracker update

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from utils import read_video, save_video
+from trackers import Tracker
+import numpy as np
 
 def main():
     # Read Video
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
-    #save video
-    save_video(video_frames, 'output_videos/output_video.mp4')
+    # Initialize tracker and extract object tracks.
+    tracker = Tracker('yolov8n.pt')
+
+    tracks = tracker.get_object_tracks(
+        video_frames,
+        read_from_stub=True,
+        stub_path='stubs/tracks_stubss.pkl',
+    )
+
+    output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control)
+    save_video(output_video_frames, 'output_videos/output_video.mp4')
 
 if __name__ == "__main__":
     main()
